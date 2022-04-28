a2f2d7506bdde7ac0a298d24636cbd64eae06915
Tamil-selvan-tring <102953614+Tamil-selvan-tring@users.noreply.github.com>
2022-04-28 13:53:05 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ul id='list'>
+      <label>Search icon<br/><li id='Searchlogo'></li></label><br/>
+      <label>Bell icon<br/><li id='Belllogo' ></li></label><br/>
+      <label>Download icon<br/><li id='Downloadlogo'></li></label><br/>
+      <label>Bookmark icon<br/><li id='Bookamarklogo'></li></label><br/>
+      <label>Mail icon<br/><li id='Maillogo'></li></label><br/></ul>
+    </div>
+  );
+}
+ 
+export default App;
